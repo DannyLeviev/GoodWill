2458e8401cbd89c2527d75ca31e67b6fc5d81b8c
DannyLeviev <danny.leviev@gmail.com>
2015-02-08 13:51:42 +0200

    Client side of the Login page.

--- app.js
+++ app.js
@@ -12,10 +12,10 @@ var app = express();
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'ejs');
 
-app.use(logger('dev'));
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(cookieParser());
+app.use(express.static(path.join(__dirname, 'bower_components')));
 app.use(express.static(path.join(__dirname, 'public')));
 
 app.use('/', routes);
@@ -39,6 +39,7 @@ if (app.get('env') === 'development') {
             error: err
         });
     });
+    app.use(logger('dev'));
 }
 
 // production error handler
